09af545cd923426d068f5b195ac6649bcc604f75
Əbil Abilli <ebilebilli3@gmail.com>
2025-02-07 18:37:00 +0400

    First version of my portfolio

---
+++ portfolio/app/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(max_length=200, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True)
