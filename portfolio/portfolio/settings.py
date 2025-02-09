a6383f18fe96172cd58b42d072ddeadc698dbe91
Əbil Abilli <ebilebilli3@gmail.com>
2025-02-10 00:34:13 +0400

    First version of portfolio

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -127,3 +127,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+#mail_settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ebilebilli3@gmail.com'  
+EMAIL_HOST_PASSWORD = 'ubyj bfpg qvbh owis' 
