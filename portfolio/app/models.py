a1bb894e701da9a73cb7e246f8e5403e5bd0aa4c
Əbil Abilli <ebilebilli3@gmail.com>
2025-02-11 01:34:01 +0400

    Model for poroject

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    name = models.TextField(max_length=100)
-    image = models.ImageField(upload_to='static/img')
-    date = models.IntegerField()
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    date = models.PositiveIntegerField()
